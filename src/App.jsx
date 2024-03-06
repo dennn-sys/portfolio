e86eb342c202231874b876fe511e036170df55f9
dennn-sys <rodenmharismael@gmail.com>
2024-03-06 15:13:04 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
     >
       <Topbar />
       <Header />
-      <div className="sticky top-0 z-10 flex w-full justify-center bg-background shadow-md">
+      <div className="sticky top-0 flex w-full justify-center bg-background shadow-md">
         <div className="w-full max-w-[1302px] px-5">
           <Navbar />
         </div>
